2e4cfc7d544a6396ddf211bcf361d5a154f6f94e
HARSHIT <apsbqt@gmail.com>
2024-04-20 14:41:04 +0530

    navbar indexing solution

---
+++ src/app/features/page.jsx
@@ -0,0 +1,13 @@
+import Card from '@/components/Card/Card'
+import Features from '@/components/Features/page'
+import React from 'react'
+
+const page = () => {
+  return (
+      <>
+          <Features />
+     </>
+  )
+}
+
+export default page
